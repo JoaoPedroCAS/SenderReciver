c8fedf2f4d6ded2d053a10f9b970895b29c68489
Joao Pedro Cardoso Amancio de Sa <70914320+JoaoPedroCAS@users.noreply.github.com>
2021-09-23 18:49:13 -0300

    Initial version, sending counter value

---
+++ Reciver.ino
@@ -0,0 +1,87 @@
+#include <SPI.h>  //responsável pela comunicação serial
+#include <LoRa.h>  //responsável pela comunicação com o WIFI Lora
+#include <Wire.h>  //responsável pela comunicação i2c
+#include "SSD1306.h" //responsável pela comunicação com o display
+
+// Definição dos pinos 
+#define SCK 5 // GPIO5 -- SX127x's SCK
+#define MISO 19 // GPIO19 -- SX127x's MISO
+#define MOSI 27 // GPIO27 -- SX127x's MOSI
+#define CS 18 // GPIO18 -- SX127x's CS
+#define RST 14 // GPIO14 -- SX127x's RESET
+#define DI00 26 // GPIO26 -- SX127x's IRQ (Interrupt Request)
+
+#define BAND 915E6 //Frequencia do radio - podemos utilizar ainda : 433E6, 868E6, 915E6
+
+SSD1306 display(0x3c, 4, 15);
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+void setup(){
+  //configura os pinos como saida
+  pinMode(16,OUTPUT); //RST do oled
+  pinMode(25,OUTPUT);
+  digitalWrite(16, LOW); // reseta o OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // enquanto o OLED estiver ligado, GPIO16 deve estar HIGH
+
+  display.init(); //inicializa o display
+  display.flipScreenVertically(); 
+  display.setFont(ArialMT_Plain_10); //configura a fonte para um tamanho maior
+
+  delay(1500);
+  display.clear(); //apaga todo o conteúdo da tela do display
+
+  SPI.begin(SCK,MISO,MOSI,CS); //inicia a comunicação serial com o Lora
+  LoRa.setPins(CS,RST,DI00); //configura os pinos que serão utlizados pela biblioteca (deve ser chamado antes do LoRa.begin)
+  display.drawString(0, 0, "OIII!");  
+  //inicializa o Lora com a frequencia específica.
+  if (!LoRa.begin(BAND)) {
+    display.drawString(0, 0, "Starting LoRa failed!");
+    display.display();
+    while (1);
+  }
+
+  //indica no display que inicilizou corretamente.
+  display.drawString(0, 0, "LoRa Initial success!");
+  display.drawString(0, 10, "Wait for incomm data...");
+  display.display();
+  delay(1000);
+
+  //LoRa.onReceive(cbk);
+  LoRa.receive(); //habilita o Lora para receber dados
+}
+
+void loop() {
+  //parsePacket: checa se um pacote foi recebido
+  //retorno: tamanho do pacote em bytes. Se retornar 0 (ZERO) nenhum pacote foi recebido
+  int packetSize = LoRa.parsePacket();
+  //caso tenha recebido pacote chama a função para configurar os dados que serão mostrados em tela
+  if (packetSize) { 
+    cbk(packetSize); //função responsável por recuperar o conteúdo do pacote recebido
+    delay(10);
+  }
+}
+
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  loraData();
+}
+
+//função responsável por configurar os dados que serão exibidos em tela.
+//RSSI : primeira linha
+//RX packSize : segunda linha
+//packet : terceira linha
+void loraData(){
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0 , 18 , "Rx "+ packSize + " bytes");
+  display.drawStringMaxWidth(0 , 39 , 128, packet);
+  display.drawString(0, 0, rssi); 
+  display.display();
+}
