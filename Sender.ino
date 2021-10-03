c9695edafcf6c3e552331df4b9e640ab606cce13
Joao Pedro Cardoso Amancio de Sa <70914320+JoaoPedroCAS@users.noreply.github.com>
2021-10-03 20:28:50 -0300

    Sensor DHT11 adicionado no Sender

--- Sender.ino
+++ Sender.ino
@@ -2,6 +2,7 @@
 #include <LoRa.h>  //responsável pela comunicação com o WIFI Lora
 #include <Wire.h>  //responsável pela comunicação i2c
 #include "SSD1306.h" //responsável pela comunicação com o display
+#include "DHT.h" //responsavel pelo sensor DHT11
 
 // Definição dos pinos 
 #define SCK 5 // GPIO5 -- SX127x's SCK
@@ -10,6 +11,8 @@
 #define CS 18 // GPIO18 -- SX127x's CS
 #define RST 14 // GPIO14 -- SX127x's RESET
 #define DI00 26 // GPIO26 -- SX127x's IRQ (Interrupt Request)
+#define DHTPIN 3 //Pino responsavel por ler os dados do DHT11
+#define DHTTYPE DHT11 //Utilizamos o DHT11
 
 #define BAND 915E6 //Frequencia do radio - podemos utilizar ainda : 433E6, 868E6, 915E6
 
@@ -18,6 +21,7 @@ unsigned int counter = 0;
 
 //parametros: address,SDA,SCL 
 SSD1306 display(0x3c, 4, 15); //construtor do objeto que controlaremos o display
+DHT dht(DHTPIN, DHTTYPE); //Construtor do objeto dht
 
 String rssi = "RSSI --";
 String packSize = "--";
@@ -25,6 +29,10 @@ String packet ;
 
 void setup()
 {
+Serial.begin(115200);
+Serial.println("Entrando no Setup");
+dht.begin(); //Iniciando o DHT11
+delay(1000);
 //configura os pinos como saida
 pinMode(16,OUTPUT); //RST do oled
 pinMode(25,OUTPUT);
@@ -52,39 +60,29 @@ while (1);
 display.drawString(0, 0, "LoRa Initial success!");
 display.display(); //mostra o conteúdo na tela
 delay(1000);
+Serial.println("Saindo do Setup");
 }
 
 void loop()
 {
-//apaga o conteúdo do display
-display.clear();
-display.setTextAlignment(TEXT_ALIGN_LEFT);
-display.setFont(ArialMT_Plain_16);
-display.drawString(0, 0, "Sending packet: ");
-display.drawString(40, 26, String(counter));
-display.display(); //mostra o conteúdo na tela
+display.clear();////apaga o conteúdo do display
+display.setTextAlignment(TEXT_ALIGN_LEFT); //Alinhamento do texto
+display.setFont(ArialMT_Plain_16); //Ajustes da fonte
+
+float h = dht.readHumidity(); //lendo a umidade
+float t = dht.readTemperature(); //lendo a temperatura
+display.drawString(0,0,"Umi: "+ String(h)+"%"); //Escrevendo o conteudo a ser mostrado no display
+display.drawString(0, 15, "Temp: "+ String(t)+"ºC");
+display.display(); //Mostrando o conteudo no display
 
 //beginPacket : abre um pacote para adicionarmos os dados para envio
 LoRa.beginPacket();
 //print: adiciona os dados no pacote
-LoRa.print("hello ");
-LoRa.print(counter);
+LoRa.print("Umidade: ");
+LoRa.println(h);
+LoRa.print("Temperatura: ");
+LoRa.println(t);
 //endPacket : fecha o pacote e envia
 LoRa.endPacket(); //retorno= 1:sucesso | 0: falha
-
-//beginPacket : abre um pacote para adicionarmos os dados para envio
-LoRa.beginPacket();
-//print: adiciona os dados no pacote
-LoRa.print("hello ");
-LoRa.print(counter);
-//endPacket : fecha o pacote e envia
-LoRa.endPacket(); //retorno= 1:sucesso | 0: falha
-
-//incrementa uma unidade no contador
-counter++;
-
-digitalWrite(25, HIGH); // liga o LED indicativo
-delay(500); // aguarda 500ms
-digitalWrite(25, LOW); // desliga o LED indicativo
-delay(500); // aguarda 500ms
+delay(60000); //Delay entre leituras de 1 minuto
 }
