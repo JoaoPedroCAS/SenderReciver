33753458d579e20aa4b5875eb93620f1160f334a
Joao Pedro Cardoso Amancio de Sa <70914320+JoaoPedroCAS@users.noreply.github.com>
2021-10-12 15:50:17 -0300

    Versão com SDS011

--- Sender.ino
+++ Sender.ino
@@ -1,8 +1,9 @@
 #include <SPI.h>  //responsável pela comunicação serial
 #include <LoRa.h>  //responsável pela comunicação com o WIFI Lora
 #include <Wire.h>  //responsável pela comunicação i2c
-#include "SSD1306.h" //responsável pela comunicação com o display
+//#include "SSD1306.h" //responsável pela comunicação com o display
 #include "DHT.h" //responsavel pelo sensor DHT11
+#include <SDS011.h> //responsavel pelo SDS011
 
 // Definição dos pinos 
 #define SCK 5 // GPIO5 -- SX127x's SCK
@@ -20,71 +21,83 @@
 unsigned int counter = 0;
 
 //parametros: address,SDA,SCL 
-SSD1306 display(0x3c, 4, 15); //construtor do objeto que controlaremos o display
+//SSD1306 display(0x3c, 4, 15); //construtor do objeto que controlaremos o display
 DHT dht(DHTPIN, DHTTYPE); //Construtor do objeto dht
+SDS011 my_sds; //construtor do objeto SDS011
 
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
+float p10, p25;
+int err,ip10,ip25;
 
-void setup()
-{
-Serial.begin(115200);
-Serial.println("Entrando no Setup");
-dht.begin(); //Iniciando o DHT11
-delay(1000);
-//configura os pinos como saida
-pinMode(16,OUTPUT); //RST do oled
-pinMode(25,OUTPUT);
-digitalWrite(16, LOW); // reseta o OLED
-delay(50); 
-digitalWrite(16, HIGH); // enquanto o OLED estiver ligado, GPIO16 deve estar HIGH
+#ifdef ESP32 //Conexão HardwareSerial dos SDS
+HardwareSerial port(2);
+#endif
 
-display.init(); //inicializa o display
-display.flipScreenVertically(); 
-display.setFont(ArialMT_Plain_10); //configura a fonte para um tamanho maior
-
-delay(1500);
-display.clear(); //apaga todo o conteúdo da tela do display
-
-SPI.begin(SCK,MISO,MOSI,CS); //inicia a comunicação serial com o Lora
-LoRa.setPins(CS,RST,DI00); //configura os pinos que serão utlizados pela biblioteca (deve ser chamado antes do LoRa.begin)
-//inicializa o Lora com a frequencia específica.
-if (!LoRa.begin(BAND))
-{
-display.drawString(0, 0, "Starting LoRa failed!");
-display.display(); //mostra o conteúdo na tela
-while (1);
+void setup(){
+  Serial.begin(115200);
+  my_sds.begin(&port);
+  Serial.println("Entrando no Setup");
+  dht.begin(); //Iniciando o DHT11
+  delay(1000);
+  //configura os pinos como saida
+  //pinMode(16,OUTPUT); //RST do oled
+  pinMode(25,OUTPUT);
+  //digitalWrite(16, LOW); // reseta o OLED
+  delay(50); 
+  //digitalWrite(16, HIGH); // enquanto o OLED estiver ligado, GPIO16 deve estar HIGH
+  
+  /*display.init(); //inicializa o display
+  display.flipScreenVertically(); 
+  display.setFont(ArialMT_Plain_10); //configura a fonte para um tamanho maior
+  
+  delay(1500);
+  display.clear(); //apaga todo o conteúdo da tela do display*/
+  
+  SPI.begin(SCK,MISO,MOSI,CS); //inicia a comunicação serial com o Lora
+  LoRa.setPins(CS,RST,DI00); //configura os pinos que serão utlizados pela biblioteca (deve ser chamado antes do LoRa.begin)
+  //inicializa o Lora com a frequencia específica.
+  if (!LoRa.begin(BAND)){
+    /*display.drawString(0, 0, "Starting LoRa failed!");
+    display.display(); //mostra o conteúdo na tela*/
+    while (1);
+  }
+  //indica no display que inicilizou corretamente.
+  //display.drawString(0, 0, "LoRa Initial success!");
+  //display.display(); //mostra o conteúdo na tela
+  delay(1000);
+  Serial.println("Saindo do Setup");
 }
-//indica no display que inicilizou corretamente.
-display.drawString(0, 0, "LoRa Initial success!");
-display.display(); //mostra o conteúdo na tela
-delay(1000);
-Serial.println("Saindo do Setup");
-}
 
-void loop()
-{
-display.clear();////apaga o conteúdo do display
-display.setTextAlignment(TEXT_ALIGN_LEFT); //Alinhamento do texto
-display.setFont(ArialMT_Plain_16); //Ajustes da fonte
-
-float h = dht.readHumidity(); //lendo a umidade
-float t = dht.readTemperature(); //lendo a temperatura
-display.drawString(0,0,"Umi: "+ String(h)+"%"); //Escrevendo o conteudo a ser mostrado no display
-display.drawString(0, 15, "Temp: "+ String(t)+"ºC");
-display.display(); //Mostrando o conteudo no display
-
-//beginPacket : abre um pacote para adicionarmos os dados para envio
-LoRa.beginPacket();
-//print: adiciona os dados no pacote
-LoRa.print("Umi: ");
-LoRa.print(h);
-LoRa.println("%");
-LoRa.print("Temp: ");
-LoRa.print(t);
-LoRa.println("ºC");
-//endPacket : fecha o pacote e envia
-LoRa.endPacket(); //retorno= 1:sucesso | 0: falha
-delay(60000); //Delay entre leituras de 1 minuto
+void loop(){
+  //display.clear();////apaga o conteúdo do display
+  //display.setTextAlignment(TEXT_ALIGN_LEFT); //Alinhamento do texto
+  //display.setFont(ArialMT_Plain_16); //Ajustes da fonte
+  float h = dht.readHumidity(); //lendo a umidade
+  float t = dht.readTemperature(); //lendo a temperatura
+  err = my_sds.read(&p25, &p10);
+  if (!err) {
+    Serial.println("Ok");
+    LoRa.beginPacket();
+    LoRa.print("Umi: ");
+    LoRa.print(h);
+    LoRa.println("%");
+    LoRa.print("Temp: ");
+    LoRa.print(t);
+    LoRa.println("ºC");
+    LoRa.print("PM25: ");
+    LoRa.println(p25);
+    LoRa.print("PM10: ");
+    LoRa.println(p10);
+    LoRa.endPacket(); //retorno= 1:sucesso | 0: falha
+  }
+  else{
+    Serial.println("Erro!");
+  }
+  //display.drawString(0,0,"Umi: "+ String(h)+"%"); //Escrevendo o conteudo a ser mostrado no display
+  //display.drawString(0, 15, "Temp: "+ String(t)+"ºC");
+  //display.display(); //Mostrando o conteudo no display
+  //beginPacket : abre um pacote para adicionarmos os dados para envio
+  //print: adiciona os dados no pacote
 }
